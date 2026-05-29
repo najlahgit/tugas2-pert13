e1b849d77ed97739a59b760e2490649dfe16cd79
unknown <najlahheho@gmail.com>
2026-05-29 20:16:06 +0700

    pertemuan13

---
+++ js_function.js
@@ -0,0 +1,37 @@
+// File: tugas2_function.js
+
+// Function kompleks dengan parameter dan return value
+function hitungTotalBelanja(namaPelanggan, listHargaItem, apakahMember) {
+    let total = 0;
+
+    // Menghitung semua item di dalam array
+    for (let i = 0; i < listHargaItem.length; i++) {
+        total += listHargaItem[i];
+    }
+
+    console.log(`Pelanggan: ${namaPelanggan}`);
+    console.log(`Total belanja kotor: Rp ${total}`);
+
+    // Logika diskon di dalam function
+    let diskon = 0;
+    if (apakahMember === true) {
+        diskon = 0.10; // Diskon member 10%
+        console.log("Mendapat diskon Member: 10%");
+    }
+
+    if (total > 500000) {
+        diskon += 0.05; // Tambahan diskon 5% jika belanja > 500rb
+        console.log("Mendapat tambahan diskon Belanja Besar: 5%");
+    }
+
+    let nilaiDiskon = total * diskon;
+    let hargaAkhir = total - nilaiDiskon;
+
+    return hargaAkhir; // Mengembalikan nilai akhir
+}
+
+// Pemanggilan function (No param / instant call diabaikan karena kita langsung pakai yang kompleks)
+let keranjangBelanjaan = [150000, 200000, 250000]; // Total 600.000
+let hasilPembayaran = hitungTotalBelanja("Aris", keranjangBelanjaan, true);
+
+console.log(`Total Akhir yang harus dibayar: Rp ${hasilPembayaran}`);
---
+++ js_if_else.js
@@ -0,0 +1,35 @@
+// File: tugas1_pemilihan.js
+
+let usia = 17;
+let hari = "Sabtu"; // Pilihan: Senin-Minggu
+let hargaDasar = 40000;
+let totalHarga = 0;
+
+console.log(`--- SISTEM PEMESANAN TIKET BIOSKOP ---`);
+console.log(`Usia Pengunjung: ${usia} tahun`);
+console.log(`Hari Kunjungan : ${hari}\n`);
+
+// Logika Pemilihan Kompleks
+if (usia < 5) {
+    console.log("Kategori: Balita. Tiket Gratis!");
+    totalHarga = 0;
+} else if (usia >= 5 && usia <= 12) {
+    console.log("Kategori: Anak-anak. Diskon 50% pada hari kerja.");
+    if (hari === "Sabtu" || hari === "Minggu") {
+        totalHarga = hargaDasar; // Tidak ada diskon di akhir pekan
+    } else {
+        totalHarga = hargaDasar * 0.5;
+    }
+} else if (usia > 12 && usia < 60) {
+    console.log("Kategori: Dewasa. Tarif Normal.");
+    if (hari === "Sabtu" || hari === "Minggu") {
+        totalHarga = hargaDasar + 15000; // Harga weekend lebih mahal
+    } else {
+        totalHarga = hargaDasar;
+    }
+} else {
+    console.log("Kategori: Lansia. Diskon Tetap 30%.");
+    totalHarga = hargaDasar * 0.7;
+}
+
+console.log(`Total yang harus dibayar: Rp ${totalHarga}`);
